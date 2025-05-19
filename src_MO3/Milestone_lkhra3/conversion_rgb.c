fb3a1e1fee353597f0b0b02fa47a2c4947a78c10
itisziyad <ziyadderghazi@gmail.com>
2025-05-19 19:48:08 +0200

    Add files via upload

--- src_MO3/Milestone_lkhra3/conversion_rgb.c
+++ src_MO3/Milestone_lkhra3/conversion_rgb.c
@@ -21,7 +21,6 @@ double**** conversion_rgb(uint8_t**** blocs_Y_Cb_Cr, int nb_blocs){
     }
 
     for (int k = 0; k < nb_blocs; k++){
-        //printf("on calcule le %deme bloc\n", k);
         for (int i = 0; i < 8; i++){
             for (int j = 0; j < 8; j++){
                 blocs_RGB[0][k][i][j] = blocs_Y_Cb_Cr[0][k][i][j] + 1.402*(blocs_Y_Cb_Cr[2][k][i][j] - 128);
@@ -51,8 +50,6 @@ uint8_t**** saturation_rgb(double**** blocs_RGB, int nbr_blocs){
         }
     }
 
-
-
     for (int p = 0; p < 3; p++ ){
         for (int k = 0; k < nbr_blocs; k++){
             for (int i = 0; i < 8; i++){
@@ -71,4 +68,27 @@ uint8_t**** saturation_rgb(double**** blocs_RGB, int nbr_blocs){
     }
 
     return blocs_rgb_sature;
+}
+
+void free_conversion_rgb(double**** blocsRGB, int nb_blocs){
+    for (int p = 0; p < 3; p++){
+        for (int i = 0; i < nb_blocs; i++){
+            free(blocs_RGB[p][i]);
+        }
+        free(blocs_RGB[p]);
+    }
+    free(blocsRGB);
+}
+
+void free_saturation_rgb(uint8_t**** blocs_rgb_sature, int nbr_blocs){
+    for (int p = 0; p < 3; p++) {
+        for (int k = 0; k < nbr_blocs; k++) {
+            for (int i = 0; i < 8; i++){
+                free(blocs_rgb_sature[p][k][i]);
+            }
+            free(blocs_rgb_sature[p][k]);
+        }
+        free(blocs_rgb_sature[p]);
+    }
+    free(blocs_rgb_sature);
 }
