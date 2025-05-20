1b366538eb19fd1cab1bc2ff37cca7c9477c4f8d
itisziyad <ziyadderghazi@gmail.com>
2025-05-20 03:02:06 +0200

    team17/ kima khasha tkon f gitlab

---
+++ team17/include/conversion_rgb.h
@@ -0,0 +1,13 @@
+#ifndef CONVERSION_RGB_H    // Garde d'inclusion : évite les inclusions multiples
+#define CONVERSION_RGB_H
+
+#include <stdint.h>
+
+double**** conversion_rgb(uint8_t**** blocs_Y_Cb_Cr, int nb_blocs);
+    /* convertit les valeurs finales de Y, Cb et Cr selon les formules données
+    vers des triplets RGB */
+uint8_t**** saturation_rgb(double**** blocs_RGB, int nbr_blocs);
+    /* sature les valeurs trouvées et renvoie des entiers prêts à utiliser */
+
+
+#endif
